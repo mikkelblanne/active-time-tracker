5cff1b0d53427ea383b17eef5064b93dfb3b3a50
Mikkel Hesselager Blanné <mkb@kmd.dk>
2020-05-27 00:58:55 +0200

    v1

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace StopWatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_manualTime = new System.Windows.Forms.Label();
+            this.button_manualTime = new System.Windows.Forms.Button();
+            this.label_unlockedTime = new System.Windows.Forms.Label();
+            this.label_unlockedDescription = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_manualTime
+            // 
+            this.label_manualTime.AutoSize = true;
+            this.label_manualTime.Location = new System.Drawing.Point(13, 13);
+            this.label_manualTime.Name = "label_manualTime";
+            this.label_manualTime.Size = new System.Drawing.Size(64, 17);
+            this.label_manualTime.TabIndex = 0;
+            this.label_manualTime.Text = "00:00:00";
+            // 
+            // button_manualTime
+            // 
+            this.button_manualTime.Location = new System.Drawing.Point(154, 10);
+            this.button_manualTime.Name = "button_manualTime";
+            this.button_manualTime.Size = new System.Drawing.Size(75, 23);
+            this.button_manualTime.TabIndex = 1;
+            this.button_manualTime.Text = "Start";
+            this.button_manualTime.UseVisualStyleBackColor = true;
+            this.button_manualTime.Click += new System.EventHandler(this.button_toggle_Click);
+            // 
+            // label_unlockedTime
+            // 
+            this.label_unlockedTime.AutoSize = true;
+            this.label_unlockedTime.Location = new System.Drawing.Point(12, 43);
+            this.label_unlockedTime.Name = "label_unlockedTime";
+            this.label_unlockedTime.Size = new System.Drawing.Size(64, 17);
+            this.label_unlockedTime.TabIndex = 0;
+            this.label_unlockedTime.Text = "00:00:00";
+            // 
+            // label_unlockedDescription
+            // 
+            this.label_unlockedDescription.AutoSize = true;
+            this.label_unlockedDescription.Location = new System.Drawing.Point(82, 43);
+            this.label_unlockedDescription.Name = "label_unlockedDescription";
+            this.label_unlockedDescription.Size = new System.Drawing.Size(144, 17);
+            this.label_unlockedDescription.TabIndex = 2;
+            this.label_unlockedDescription.Text = "Screen unlocked time";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button_manualTime;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 89);
+            this.Controls.Add(this.label_unlockedDescription);
+            this.Controls.Add(this.button_manualTime);
+            this.Controls.Add(this.label_unlockedTime);
+            this.Controls.Add(this.label_manualTime);
+            this.Name = "Form1";
+            this.Text = "StopWatch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_manualTime;
+        private System.Windows.Forms.Button button_manualTime;
+        private System.Windows.Forms.Label label_unlockedTime;
+        private System.Windows.Forms.Label label_unlockedDescription;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace StopWatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            StartAutoCounter();
+        }
+
+        private int _manualSecondsElapsed;
+        private bool _manualEnabled;
+        private readonly Timer _manualTimer = new Timer();
+
+        private void button_toggle_Click(object sender, EventArgs e)
+        {
+            if (_manualEnabled)
+            {
+                _manualEnabled = false;
+                _manualTimer.Stop();
+                _manualTimer.Tick -= ManualTimerOnTick;
+                button_manualTime.Text = "Start";
+            }
+            else
+            {
+                _manualEnabled = true;
+                _manualTimer.Interval = 1000;
+                _manualTimer.Tick += ManualTimerOnTick;
+                _manualTimer.Start();
+                button_manualTime.Text = "Stop";
+            }
+        }
+
+        private void ManualTimerOnTick(object sender, EventArgs e)
+        {
+            _manualSecondsElapsed += 1;
+            TimeSpan span = TimeSpan.FromSeconds(_manualSecondsElapsed);
+            label_manualTime.Text = span.ToString();
+        }
+
+        private TimeSpan _unlockedTimeElapsed = TimeSpan.Zero;
+        private DateTime _lastUnlock;
+        private readonly Timer _alwaysTimer = new Timer();
+
+        private void StartAutoCounter()
+        {
+            SystemEvents.SessionSwitch += SystemEventsOnSessionSwitch;
+
+            _lastUnlock = DateTime.Now;
+            _alwaysTimer.Interval = 1000;
+            _alwaysTimer.Tick += _autoTimer_Tick;
+            _alwaysTimer.Start();
+        }
+
+        private void _autoTimer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan elapsed = _unlockedTimeElapsed;
+            elapsed += DateTime.Now - _lastUnlock;
+            label_unlockedTime.Text = elapsed.ToString("hh\\:mm\\:ss");
+        }
+
+        private void SystemEventsOnSessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            switch (e.Reason)
+            {
+                case SessionSwitchReason.SessionLock:
+                    TimeSpan currentElapsed = DateTime.Now - _lastUnlock;
+                    Debug.WriteLine($"SessionSwitch: Locked after {currentElapsed}");
+                    _unlockedTimeElapsed += currentElapsed;
+                    break;
+                case SessionSwitchReason.SessionUnlock:
+                    _lastUnlock = DateTime.Now;
+                    Debug.WriteLine($"SessionSwitch: Unlocked at {_lastUnlock}");
+                    break;
+                default:
+                    Debug.WriteLine($"SessionSwitch: {e.Reason}");
+                    break;
+            }
+        }
+    }
+}
